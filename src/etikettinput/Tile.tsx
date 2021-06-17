ff17121988e3f0c4f5189a38181fafa0d9728b88
RomeoBhuiyan <55655775+Discord-Chan@users.noreply.github.com>
2021-06-17 15:22:40 +0200

    added website name

--- src/etikettinput/Tile.tsx
+++ src/etikettinput/Tile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 import { DeleteBTN, SaveBTN } from '../components/Buttons';
 import { Checkbox, Input } from '../components/Forms';
@@ -154,3 +155,33 @@ export const InputTile = () => (
     </div>
   </div>
 );
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
+// Create Document Component
+export const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Absender: <br></br>
+              {{}}
+        </Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
