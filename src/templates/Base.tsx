d9e1cda0aabf6f456f7c7e03ca2035de996fc6a7
Martin-Hausleitner <s.m@hausleitner.eu>
2021-06-18 10:46:00 +0200

    fix for build

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 import { PrintBTN, DownloadBTN, NewBTN } from '../components/Buttons';
 import { Tile, InputTile } from '../etikettinput/Tile';
